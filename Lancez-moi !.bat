2fa3b1450ce881968bc376ff1ab880b70c2ccb25
guillaumeayad <atoualbi@sve-universite.com>
2022-06-14 12:51:14 +0200

    bat modification

--- Lancez-moi !.bat
+++ Lancez-moi !.bat
@@ -1,11 +1,11 @@
 
 @Echo off
 
-SET mypath="\\SRV-DC01.sve.local\Sve_Datas\INFORMATIQUE\IT\Mise en Page Excel"
+@REM SET mypath="\\SRV-DC01.sve.local\Sve_Datas\INFORMATIQUE\IT\Mise en Page Excel"
 
-pushd %mypath%
+@REM pushd %mypath%
 echo Lancement de l'application, merci de patienter...
-rem cd /d %mypath%
+@REM rem cd /d %mypath%
 CALL .\notebookenv\Scripts\activate.bat
 python "main.py"
-popd
+@REM popd
